7bbab77587663d07ba14afbbf026f8e39aa4ecfc
Zhongwei Li <zhongweili95@gmail.com>
2021-04-29 15:31:33 -0400

    implementing the main algorithm

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-let arr = process.argv.slice(2)
+const arr = process.argv.slice(2);
+const dict = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+};
 
 let result = "";
-arr.forEach(function(num) {
+arr.forEach(function (num) {
     result += toPhonetic(num);
-    if (num != arr[arr.length-1]) result += ",";
+    if (num !== arr[arr.length - 1]) result += ",";
 });
+
+function toPhonetic(num) {
+    let phonetic = "";
+    for (const digit of num) {
+        if (!dict[digit]) return "invalid";
+        phonetic += dict[digit];
+    }
+    return phonetic;
+}
+
+console.log(result);
